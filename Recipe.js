9a2f7952d0e901246f17f28d7f19572dd149ed85
Kevin Roney <karoney@dmacc.edu>
2023-04-27 08:09:53 -0500

    added JS Class Recipe

---
+++ Recipe.js
@@ -0,0 +1,12 @@
+export class Recipe {
+    constructor(recipeName, numberOfServings, prepTime, cookTime, dicciculty, ingredientList, instructionList, recipePhoto){
+        this.recipeName = recipeName
+        this.numberOfServings = numberOfServings
+        this.prepTime = prepTime
+        this.cookTime = cookTime
+        this.difficulty = difficulty
+        this.ingredientList = ingredientList
+        this.instructionList = instructionList
+        this.recipePhoto = recipePhoto
+    }
+}
